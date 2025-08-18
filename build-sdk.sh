93dedd36c7ba7722a2ba59a86e678d4d315ec5c1
Martin <mautner@vrvis.at>
2025-08-18 17:05:11 +0200

    Add build-sdk.sh

---
+++ build-sdk.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+git submodule update --init --recursive external/omm-sdk
+cd external
+
+mkdir -p support/scripts
+cp omm-sdk/support/scripts/postinstall.cmake support/scripts/postinstall.cmake
+rm -rf build
+
+cmake -DCMAKE_BUILD_TYPE=Release -DOMM_ENABLE_TESTS=off -DOMM_BUILD_VIEWER=off -DOMM_INTEGRATION_LAYER_NVRHI=off -DOMM_ENABLE_PRECOMPILED_SHADERS_DXIL=off -S . -B build
+cmake --build build --config Release
+cmake --install build --config Release
+
+rm -rf support
